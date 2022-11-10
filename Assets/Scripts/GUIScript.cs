85d8bec469c2501f37e837cbf3fb084cbdf45bdb
Harley <hajeffries8288@student.pps.net>
2022-11-10 07:05:21 -0800

    Development build info

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -6,13 +6,33 @@ using UnityEngine.UI;
 public class GUIScript : MonoBehaviour
 {
     //UpdateResorces
+    [Header("Resorces")]
     public static Text resorcesText;
+
+    //DevelopmentBuild
+    public GameObject developmentBuild;
+    public string versionNumber;
+    public int buildNumber;
+
+    public static Text version;
+    public static Text build;
 
     void Start() //Start is called before the first frame update
     {
         //UpdateResorces
         resorcesText = GameObject.Find("Resorces").GetComponent<Text>();
         resorcesText.text = "Resorces: " + TractorBeemScript.astroyidsCollected;
+
+        //DevelopmentBuild
+        if (Debug.isDebugBuild)
+        {
+            developmentBuild.SetActive(true);
+
+            version = GameObject.Find("Version").GetComponent<Text>();
+            version.text = "Version: " + versionNumber;
+            build = GameObject.Find("Build").GetComponent<Text>();
+            build.text = "Build: " + buildNumber;
+        }
     }
 
     void Update() //Update is called once per frame
